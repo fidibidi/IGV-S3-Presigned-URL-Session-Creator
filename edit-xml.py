4b40c62b6a1c1169393e69a1e3bdd8c2519d0306
fidibidi <fidel.nagy@gmail.com>
2022-02-16 14:47:26 -0700

    added working xmlFileClass

--- edit-xml.py
+++ edit-xml.py
@@ -190,87 +190,14 @@ class S3SamplesManager:
             def __repr__(self):
                 return f'Filename: {self.filename}, Index: {self.index}, Path: {self.path}, Type: {self.type}'
 
-            def __init__(self, filename='M0354.sorted.bam', path='https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0354.sorted.bam?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215431Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=3a77f44970b68c86a2b6f183744a9e7960162b80cfdd2c9fcdb81cf3563e5c43', type='bam', index='https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0354.sorted.bam.bai?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215432Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=fc1917cccdb8eec64c8163dbb9877bdf59ada8300aa2d945a98d6a3aa62dd6fd'):
+            def __init__(self, filename='M0356.sorted.bam', path='https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0356.sorted.bam?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220216T211445Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=721f65941799c60a51d2f11d7721ff362755778414e20cd233c036c7d472156f', type='bam', index='https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0356.sorted.bam.bai?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220216T211449Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=920cabc890f8e13056c61552cf80753f078d7182618e3d3db9434df45355bbda'):
                 self.filename = filename
                 self.index = index
                 self.path = path
                 self.type = type
 
 
-
-demo_files_dict = [
-    [
-        {
-            "filename": "M0354.sorted.bam",
-            "index": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0354.sorted.bam.bai?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215432Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=fc1917cccdb8eec64c8163dbb9877bdf59ada8300aa2d945a98d6a3aa62dd6fd",
-            "path": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0354.sorted.bam?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215431Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=3a77f44970b68c86a2b6f183744a9e7960162b80cfdd2c9fcdb81cf3563e5c43",
-            "type": "bam"
-        },
-        {
-            "filename": "M0354.cuteSV.vcf",
-            "path": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/vcfs/cuteSV/M0354.cuteSV.vcf?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215802Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=8b6d2171cc291f062483089f36cf23dbc82a83c1d9a7516f0f99ce4e5abe8b06",
-            "type": "vcf"
-        }
-    ],
-    [
-        {
-            "filename": "M0356.sorted.bam",
-            "index": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0356.sorted.bam.bai?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215806Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=b8308a56a2a69675af5d2c3d3fd426aa44ff3754652c3c4bf1ced992a9152584",
-            "path": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0356.sorted.bam?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215806Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=80d0859d5202ccf34aaca23acf4133cea1a4ba19d50a851d8e84f76a123ec26c",
-            "type": "bam"
-        },
-        {
-            "filename": "M0356.cuteSV.vcf",
-            "path": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/vcfs/cuteSV/M0356.cuteSV.vcf?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215808Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=06b1e3eb7f067f741b4c243cfc6875acab5385f1432409d0ed2fe7d92be10469",
-            "type": "vcf"
-        }
-    ]
-]
-
-demo_files_class_dict = [
-    [
-        IGVFile(
-            filename="M0354.sorted.bam",
-            index="https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0354.sorted.bam.bai?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215432Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=fc1917cccdb8eec64c8163dbb9877bdf59ada8300aa2d945a98d6a3aa62dd6fd",
-            path="https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0354.sorted.bam?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215431Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=3a77f44970b68c86a2b6f183744a9e7960162b80cfdd2c9fcdb81cf3563e5c43",
-            type="bam"
-        ),
-        IGVFile(
-            filename="M0354.cuteSV.vcf",
-            path="https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/vcfs/cuteSV/M0354.cuteSV.vcf?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215802Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=8b6d2171cc291f062483089f36cf23dbc82a83c1d9a7516f0f99ce4e5abe8b06",
-            type="vcf"
-        ),
-        IGVFile(
-            filename="M0354.clair3.vcf.gz",
-            path="https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/vcfs/clair3/M0354.clair3.vcf.gz?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215801Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=c8368375d97162e42eef2e3e9d551213a6c86244bda278d1e65519bcb9947350",
-            index="https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/vcfs/clair3/M0354.clair3.vcf.gz.tbi?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215802Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=9a05592eff59038e2f4dfeb4ce9693b4ec9fe7a3498c764fd038cb4ec161996d",
-            type="vcf"
-        )
-    ]
-]
-
-# print(demo_files_dict[0][0])
-
-# text = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?> 
-# <Session genome="hg38" hasGeneTrack="true" hasSequenceTrack="true" locus="chr1:237334302-237369372" nextAutoscaleGroup="2" version="8">
-#     <Resources>
-#         ''' + make_resources(demo_files_class_dict) + '''
-#     </Resources>
-#     ''' + make_sample_panels(demo_files_class_dict) + '''
-#     <Panel height="54" name="FeaturePanel" width="1311">
-#         <Track attributeKey="Reference sequence" clazz="org.broad.igv.track.SequenceTrack" fontSize="10" id="Reference sequence" name="Reference sequence" sequenceTranslationStrandValue="POSITIVE" shouldShowTranslation="false" visible="true"/>
-#         <Track attributeKey="Gene" clazz="org.broad.igv.track.FeatureTrack" color="0,0,178" colorScale="ContinuousColorScale;0.0;845.0;255,255,255;0,0,178" fontSize="10" groupByStrand="false" height="35" id="hg38_genes" name="Gene" visible="true"/>
-#     </Panel>
-#     <PanelLayout dividerFractions="0.2126642771804062,0.4336917562724014,0.6798088410991637,0.929510155316607"/>
-#     <HiddenAttributes>
-#         <Attribute name="DATA FILE"/>
-#         <Attribute name="DATA TYPE"/>
-#         <Attribute name="NAME"/>
-#     </HiddenAttributes>
-# </Session>'''
-
-
-def run():
+def string_main():
     if not path.exists('data.pl'):
         sampleManager = S3SamplesManager()
         sampleManager.start()
@@ -306,55 +233,61 @@ def run():
     fileTree.write('edit-xml-2.xml')
 
 
-def indent(elem, level=0):
-    i = "\n" + level*"\t"
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "\t"
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-        for elem in elem:
-            indent(elem, level+1)
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-    else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
+class xmlManager:
+    def __init__(self):
+        self.mainTree = ET.parse('xml-template.xml')
+        self.root = self.mainTree.getroot()
+
+    def indent(self, elem, level=0):
+        i = "\n" + level*"\t"
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = i + "\t"
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+            for elem in elem:
+                indent(elem, level+1)
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
+
+    def save(self, output='test.xml'):
+        
+        self.indent(self.root)
+        self.mainTree.write(output)
+
+    def addResources(self, igvFile):
+        resources = self.mainTree.find('Resources')
+
+        resource = ET.SubElement(resources, 'Resource')
+        resource.set('index', igvFile.index)
+        resource.set('path', igvFile.path)
+
+    def addPanelWithTrack(self, igvFile):
+        panel = ET.Element("Panel", Panel = "300", name = "DataPanel", width = "1311")
+        coverageTrack = ET.SubElement(panel, "Track", attributeKey=f'{igvFile.filename} Coverage', autoScale="true", clazz="org.broad.igv.sam.CoverageTrack", color="175,175,175", colorScale="ContinuousColorScale;0.0;60.0;255,255,255;175,175,175", fontSize="10", id=f"{igvFile.path}_coverage", name=f'{igvFile.filename} Coverage', snpThreshold="0.2", visible="true")
+        datarange = ET.SubElement(coverageTrack, "DataRange", baseline="0.0", drawBaseline="true", flipAxis="false", maximum="60.0", minimum="0.0", type="LINEAR")
+        
+        junctionTrack = ET.SubElement(panel, "Track", attributeKey=f'{igvFile.filename} Junctions', clazz="org.broad.igv.sam.SpliceJunctionTrack", fontSize="10", groupByStrand="false", height="60", id=f"{igvFile.path}_junctions", name=f"{igvFile.filename} Junctions", visible="false")
+        alignmentTrack = ET.SubElement(panel, "Track", attributeKey=f'{igvFile.filename}', clazz="org.broad.igv.sam.AlignmentTrack", displayMode="EXPANDED", experimentType="THIRD_GEN", fontSize="10", id=f"{igvFile.path}", name=f"{igvFile.filename}", visible="false")
+        renderOptions = ET.SubElement(alignmentTrack, "RenderOptions")
+
+        self.root.insert(1, panel)
+
+    def processFile(self, igvFile):
+        self.addResources(igvFile)
+        self.addPanelWithTrack(igvFile)
+
+
 
 def main():
+    xmlFile = xmlManager()
+    IGVFileObj = S3SamplesManager.S3Sample.IGVFile()
+    xmlFile.processFile(IGVFileObj)    
+    xmlFile.save()
 
-    # ADD Panels
-    tempObj = {
-        "filename": "M0356.sorted.bam",
-        "index": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0356.sorted.bam.bai?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220216T211449Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=920cabc890f8e13056c61552cf80753f078d7182618e3d3db9434df45355bbda",
-        "path": "https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/bams/M0356.sorted.bam?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220216T211445Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=721f65941799c60a51d2f11d7721ff362755778414e20cd233c036c7d472156f",
-        "type": "bam"
-    }
-
-    # Add Resources
-    mainTree = ET.parse('xml-template.xml')
-    resources = mainTree.find('Resources')
-
-    resource = ET.SubElement(resources, 'Resource')
-    resource.set('index', tempObj.get('index'))
-    resource.set('path', tempObj.get('path'))
- 
-    root = mainTree.getroot()
-    panel = ET.Element("Panel", Panel = "300", name = "DataPanel", width = "1311")
-
-    coverageTrack = ET.SubElement(panel, "Track", attributeKey=f'{tempObj.get("filename")} Coverage', autoScale="true", clazz="org.broad.igv.sam.CoverageTrack", color="175,175,175", colorScale="ContinuousColorScale;0.0;60.0;255,255,255;175,175,175", fontSize="10", id=f"{tempObj.get('path')}_coverage", name=f'{tempObj.get("filename")} Coverage', snpThreshold="0.2", visible="true")
-    datarange = ET.SubElement(coverageTrack, "DataRange", baseline="0.0", drawBaseline="true", flipAxis="false", maximum="60.0", minimum="0.0", type="LINEAR")
-    
-    junctionTrack = ET.SubElement(panel, "Track", attributeKey=f'{tempObj.get("filename")} Junctions', clazz="org.broad.igv.sam.SpliceJunctionTrack", fontSize="10", groupByStrand="false", height="60", id=f"{tempObj.get('path')}_junctions", name=f"{tempObj.get('filename')} Junctions", visible="false")
-    alignmentTrack = ET.SubElement(panel, "Track", attributeKey=f'{tempObj.get("filename")}', clazz="org.broad.igv.sam.AlignmentTrack", displayMode="EXPANDED", experimentType="THIRD_GEN", fontSize="10", id=f"{tempObj.get('path')}", name=f"{tempObj.get('filename')}", visible="false")
-    renderOptions = ET.SubElement(alignmentTrack, "RenderOptions")
-
-    root.insert(1, panel)
-    indent(root)
-
-    mainTree.write('test.xml')
-
-    
 
 
 if __name__ == "__main__":

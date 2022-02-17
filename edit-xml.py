640ee1240af55c996bcaf5311801678e9088dc1a
fidibidi <fidel.nagy@gmail.com>
2022-02-17 10:35:16 -0700

    Working manual entry + regen

--- edit-xml.py
+++ edit-xml.py
@@ -23,109 +23,27 @@ sys.path.append(".")
 def bash_command(cmd):
     return sp.Popen(cmd, shell=True, executable='/bin/bash', stdout=subprocess.PIPE)
 
+def string_prompt(message):
+    val = input(message)
+    if type(val) == str:
+        return val
+    else:
+        string_prompt("Please enter string values only.")
 
-def make_bam_track(sample, url):
-    track = '''    <Track attributeKey="''' + sample + ''' Coverage" autoScale="true" clazz="org.broad.igv.sam.CoverageTrack" color="175,175,175" colorScale="ContinuousColorScale;0.0;60.0;255,255,255;175,175,175" fontSize="10" id="''' + url + '''_coverage" name="''' + sample + ''' Coverage" snpThreshold="0.2" visible="true">
-            <DataRange baseline="0.0" drawBaseline="true" flipAxis="false" maximum="60.0" minimum="0.0" type="LINEAR"/>
-        </Track>
-        <Track attributeKey="''' + sample + ''' Junctions" clazz="org.broad.igv.sam.SpliceJunctionTrack" fontSize="10" groupByStrand="false" height="60" id="''' + url + '''_junctions" name="''' + sample + ''' Junctions" visible="false"/>
-        <Track attributeKey="''' + sample + '''" clazz="org.broad.igv.sam.AlignmentTrack" displayMode="EXPANDED" experimentType="THIRD_GEN" fontSize="10" id="''' + url + '''" name="''' + sample + '''" visible="false">
-            <RenderOptions/>
-        </Track>'''
-    return track
-
-
-def make_vcf_track(fileName, url):
-    track = '''<Track attributeKey="''' + fileName + '''" clazz="org.broad.igv.variant.VariantTrack" displayMode="EXPANDED" featureVisibilityWindow="100000" fontSize="10" groupByStrand="false" id="''' + \
-        url + '''" name="''' + fileName + \
-            '''" siteColorMode="ALLELE_FREQUENCY" squishedHeight="1" visible="true"/>'''
-    # track = '''<Track attributeKey="M0354.cuteSV.vcf" clazz="org.broad.igv.variant.VariantTrack" displayMode="EXPANDED" fontSize="10" groupByStrand="false" id="https://praxisgenomics-patient-res.s3.us-east-1.amazonaws.com/nanopore/rando-ONT/vcfs/cuteSV/M0354.cuteSV.vcf?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIASEJWMTN76OTW345Z%2F20220209%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-Date=20220209T215802Z&amp;X-Amz-Expires=604800&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=8b6d2171cc291f062483089f36cf23dbc82a83c1d9a7516f0f99ce4e5abe8b06" name="M0354.cuteSV.vcf" siteColorMode="ALLELE_FREQUENCY" squishedHeight="1" visible="true"/>'''
-    return track
-
-
-def return_track(track_list):
-    combined_tracks = ""
-    for track in track_list:
-        if not combined_tracks:
-            combined_tracks = track
-        else:
-            combined_tracks = f'''{combined_tracks}
-        {track}'''
-    return combined_tracks
-
-
-def make_panel(track_list):
-    header = '''<Panel height="395" name="DataPanel" width="1311">'''
-    closer = '''</Panel>'''
-    final_panel = f'''{header}
-    {return_track(track_list)}
-    {closer}'''
-    return final_panel
-
-
-def make_sample_panels(samples_list):
-    file_panels = []
-    for file_list in samples_list:
-        file_tracks = []
-        for files_object in file_list:
-            file_track = ""
-            if files_object.type == "bam":
-                file_track = make_bam_track(
-                    files_object.filename, files_object.path)
-            if files_object.type == "vcf":
-                file_track = make_vcf_track(
-                    files_object.filename, files_object.path)
-            file_tracks.append(file_track)
-        file_panels.append(file_tracks)
-    # iterate through and combine...
-
-    final_panels = ""
-    for panel in file_panels:
-        if not final_panels:
-            final_panels = make_panel(panel)
-        else:
-            # final_panels = final_panels + "\n" + make_panel(panel)
-            final_panels = f'''{final_panels}
-    {make_panel(panel)}'''
-
-    return final_panels
-
-
-def make_resource(sample_dict):
-    index = sample_dict.index
-    index = (f'index="{index}" ' if sample_dict.index else '')
-
-    path = sample_dict.path
-    path = (f'path="{path}"' if sample_dict.path else '')
-
-    resource = '''<Resource ''' + index + path + '''/>'''
-    return resource
-
-
-def make_resources(sample_list):
-    final_resources = ""
-    for files_list in sample_list:
-        for files_object in files_list:
-            resource = make_resource(files_object)
-            if resource:
-                if not final_resources:
-                    final_resources = f'''{resource}'''
-                else:
-                    final_resources = f'''{final_resources}
-        {resource}'''
-    return final_resources
-
-
-class IGVFile:
-    def __init__(self, filename, path, type, index=''):
-        self.filename = filename
-        self.index = index
-        self.path = path
-        self.type = type
+def bool_prompt(message):
+    val = input(message)
+    if val.strip().lower() == 'y':
+        return True
+    elif val.strip().lower() == 'n':
+        return False
+    else:
+        print('Error, please type y or n')
+        bool_prompt(message)
 
 class S3SamplesManager:
-    def __init__(self):
+    def __init__(self, saveFile='data.pl'):
         self.S3Samples = []
+        self.saveFile = saveFile
 
     def bool_prompt(self, message):
         val = input(message)
@@ -141,7 +59,7 @@ class S3SamplesManager:
         self.S3Samples.append(s3Sample)
 
     def start(self, s3Client):
-        if (self.bool_prompt("Add sample?: ")):
+        if (self.bool_prompt("Add sample? (y/n):\n")):
             s3SampleObj = self.S3Sample()
             s3SampleObj.start(s3Client)
             
@@ -153,7 +71,14 @@ class S3SamplesManager:
     class S3Sample:
 
         def __init__(self):
-            self.IGVFiles = []
+            self.IGVFiles = []            
+
+        def string_prompt(self, message):
+            val = input(message)
+            if type(val) == str:
+                return val
+            else:
+                self.string_prompt("Please enter string values only.\n")
 
         def __bool_prompt(self, message):
             val = input(message)
@@ -164,59 +89,53 @@ class S3SamplesManager:
             else:
                 print('Error, please type y or n')
                 self.__bool_prompt(message)
-
-        def __string_prompt(self, message):
-            val = input(message)
-            if type(val) == str:
-                return val
-            else:
-                self.__string_prompt("Please enter string values only.")
         
         def start(self, s3Client):
-            if self.__bool_prompt("Add file for sample?: "):
-                # filename = self.__string_prompt("Enter filename: ")
-                # type = self.__string_prompt("Type (VCF, BAM): ")
-                # print("Enter File URL: ")
-                # print("(ex. s3://praxisgenomics-patient-res/novaseq/CA0402/RES123121/e0e4956a-ad90-4cdf-9451-685aff26293f/$SAMPLE.bam)")
-                # url = self.__string_prompt("")
-                # indexUrl = self.__string_prompt("Enter File Index URL: ")
-                filename = "M0354.sorted.bam"
-                type = "bam"
-                url = "s3://praxisgenomics-patient-res/nanopore/rando-ONT/bams/M0354.sorted.bam"
-                indexUrl = "s3://praxisgenomics-patient-res/nanopore/rando-ONT/bams/M0354.sorted.bam.bai"
-                path = self.createPresign(url, s3Client)
-                index = self.createPresign(indexUrl, s3Client)
+            if self.__bool_prompt("Add file for sample? (y/n):\n "):
+                type = self.string_prompt("Type (VCF, BAM):\n ")
+                filename = self.string_prompt("Enter filename:\n ")
+                print("Enter File URL: ")
+                print("(ex. s3://praxisgenomics-patient-res/novaseq/CA0402/RES123121/e0e4956a-ad90-4cdf-9451-685aff26293f/$SAMPLE.bam)")
+                url = self.string_prompt("")
+                indexUrl = self.string_prompt("Enter File Index URL: ")
+
+                path = self.createPresign(url, s3Client) 
+                index = self.createPresign(indexUrl, s3Client) if indexUrl else ""
                 sample = self.IGVFile(filename=filename, url=url, indexUrl=indexUrl, path=path, index=index, type=type)
                 self.IGVFiles.append(sample)
                 
                 self.start(s3Client)
 
         def createPresign(self, link, s3Client):
-            link = link.split('/',3)
-            bucket = link[2]
-            key = link[3]
+            if link:
+                link = link.split('/',3)
+                bucket = link[2]
+                key = link[3]
 
-            try:
-                response = s3Client.generate_presigned_url('get_object', Params={'Bucket': bucket,'Key': key},ExpiresIn=604800)
-            except ClientError as e:
-                logging.error(e)
-                return None
+                try:
+                    response = s3Client.generate_presigned_url('get_object', Params={'Bucket': bucket,'Key': key},ExpiresIn=600)
+                except ClientError as e:
+                    logging.error(e)
+                    return ""
 
-            # The response contains the presigned URL
-            return response
+                return response
+            else:
+                logging.warning("createPresign: No link was passed.")
+                return ""
 
         def updateLinks(self, s3Client):
             for IGVFileObj in self.IGVFiles:
                 IGVFileObj.path = self.createPresign(IGVFileObj.url, s3Client)
-                IGVFileObj.index = self.createPresign(IGVFileObj.indexUrl, s3Client)
+                if IGVFileObj.indexUrl:
+                    IGVFileObj.index = self.createPresign(IGVFileObj.indexUrl, s3Client)
 
         def returnS3Sample(self):
-            return self.IGVFiles
+            return self
 
 
         class IGVFile:
             def __repr__(self):
-                return f'Filename: {self.filename}, Index: {self.index}, Path: {self.path}, Type: {self.type}'
+                return f'Filename: {self.filename}, Index: {self.index}, Path: {self.path}, Type: {self.type}, URL: {self.url}, Index URL: {self.indexUrl}'
 
             def __init__(self, filename='', url='', indexUrl='', path='', index='', type=''):
                 self.filename = filename
@@ -225,42 +144,6 @@ class S3SamplesManager:
                 self.type = type
                 self.url = url
                 self.indexUrl = indexUrl
-
-def string_main():
-    if not path.exists('data.pl'):
-        sampleManager = S3SamplesManager()
-        sampleManager.start()
-        dataFile = open('data.pl', 'ab')
-        pickle.dump(sampleManager, dataFile)
-        dataFile.close()
-    else:
-        print("DATA EXISTS")
-        dataFile = open('data.pl', 'rb')
-        pickleData = pickle.load(dataFile)
-        sampleManager = pickleData
-        dataFile.close()
-
-    text = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<Session genome="hg38" hasGeneTrack="true" hasSequenceTrack="true" locus="chr1:237334302-237369372" nextAutoscaleGroup="2" version="8">
-    <Resources>
-        ''' + make_resources(sampleManager.S3Samples) + '''
-    </Resources>
-    ''' + make_sample_panels(sampleManager.S3Samples) + '''
-    <Panel height="54" name="FeaturePanel" width="1311">
-        <Track attributeKey="Reference sequence" clazz="org.broad.igv.track.SequenceTrack" fontSize="10" id="Reference sequence" name="Reference sequence" sequenceTranslationStrandValue="POSITIVE" shouldShowTranslation="false" visible="true"/>
-        <Track attributeKey="Gene" clazz="org.broad.igv.track.FeatureTrack" color="0,0,178" colorScale="ContinuousColorScale;0.0;845.0;255,255,255;0,0,178" fontSize="10" groupByStrand="false" height="35" id="hg38_genes" name="Gene" visible="true"/>
-    </Panel>
-    <PanelLayout dividerFractions="0.2126642771804062,0.4336917562724014,0.6798088410991637,0.929510155316607"/>
-    <HiddenAttributes>
-        <Attribute name="DATA FILE"/>
-        <Attribute name="DATA TYPE"/>
-        <Attribute name="NAME"/>
-    </HiddenAttributes>
-</Session>'''
-
-    fileTree = ET.ElementTree(ET.fromstring(text))
-    fileTree.write('edit-xml-2.xml')
-
 
 class xmlManager:
     def __init__(self):
@@ -283,7 +166,6 @@ class xmlManager:
                 elem.tail = i
 
     def save(self, output='test.xml'):
-        
         self.indent(self.root)
         self.mainTree.write(output)
 
@@ -291,11 +173,11 @@ class xmlManager:
         resources = self.mainTree.find('Resources')
 
         resource = ET.SubElement(resources, 'Resource')
-        resource.set('index', igvFile.index)
+        if igvFile.index: 
+            resource.set('index', igvFile.index)
         resource.set('path', igvFile.path)
 
-    def addPanelWithTrack(self, igvFile):
-        panel = ET.Element("Panel", Panel = "300", name = "DataPanel", width = "1311")
+    def addPanelWithBamTrack(self, igvFile, panel):
         coverageTrack = ET.SubElement(panel, "Track", attributeKey=f'{igvFile.filename} Coverage', autoScale="true", clazz="org.broad.igv.sam.CoverageTrack", color="175,175,175", colorScale="ContinuousColorScale;0.0;60.0;255,255,255;175,175,175", fontSize="10", id=f"{igvFile.path}_coverage", name=f'{igvFile.filename} Coverage', snpThreshold="0.2", visible="true")
         datarange = ET.SubElement(coverageTrack, "DataRange", baseline="0.0", drawBaseline="true", flipAxis="false", maximum="60.0", minimum="0.0", type="LINEAR")
         
@@ -303,35 +185,83 @@ class xmlManager:
         alignmentTrack = ET.SubElement(panel, "Track", attributeKey=f'{igvFile.filename}', clazz="org.broad.igv.sam.AlignmentTrack", displayMode="EXPANDED", experimentType="THIRD_GEN", fontSize="10", id=f"{igvFile.path}", name=f"{igvFile.filename}", visible="false")
         renderOptions = ET.SubElement(alignmentTrack, "RenderOptions")
 
-        self.root.insert(1, panel)
+
+    def addPanelWithVcfTrack(self, igvFile, panel):
+        vcfTrack = ET.SubElement(panel, "Track", attributeKey=f'{igvFile.filename}', clazz="org.broad.igv.variant.VariantTrack", displayMode="COLLAPSED", fontSize="10", groupByStrand="false", id=f'{igvFile.path}', name=f'{igvFile.filename}', siteColorMode="ALLELE_FREQUENCY", squishedHeight="1", visible="true")
 
     def processFile(self, igvFile):
         self.addResources(igvFile)
-        self.addPanelWithTrack(igvFile)
+        if igvFile.type == "bam":
+            self.addPanelWithBamTrack(igvFile)
+        elif igvFile.type == "vcf":
+            self.addPanelWithVcfTrack(igvFile)
 
+    def processFiles(self, S3Sample, panelID):
+        panel = ET.Element("Panel", Panel="300", name=f"{panelID}", width="1311")
+        for igvFileObj in S3Sample.IGVFiles:
+            self.addResources(igvFileObj)
+            if igvFileObj.type == "bam":
+                self.addPanelWithBamTrack(igvFileObj, panel)
+            elif igvFileObj.type == "vcf":
+                self.addPanelWithVcfTrack(igvFileObj, panel)
 
+        self.root.insert(1, panel)
+
+    def processSampleManagerSamples(self, sampleManager):
+
+        PanelInd = 1
+        for S3Sample in sampleManager.S3Samples:
+            if S3Sample == sampleManager.S3Samples[-1]:
+                panelID = "DataPanel"
+            else:
+                panelID = f'Panel{PanelInd}'
+            self.processFiles(S3Sample, panelID)
+            PanelInd += 1
+    
+    def processJSONSamples(self, JSONData):
+        pass
 
 def main():
     xmlFile = xmlManager()
     s3Client = b3.client('s3')
-    if not path.exists('data.pl'):
-        sampleManager = S3SamplesManager()
-        sampleManager.start(s3Client)
-        dataFile = open('data.pl', 'ab')
-        pickle.dump(sampleManager, dataFile)
-        dataFile.close()
-    else:
-        dataFile = open('data.pl', 'rb')
-        pickleData = pickle.load(dataFile)
-        sampleManager = pickleData
-        dataFile.close()
 
-    for S3Sample in sampleManager.S3Samples:
-        for IGVFileObj in S3Sample:
-            xmlFile.processFile(IGVFileObj)    
+    if bool_prompt("Create new IGV session? (y/n):\n"):
+        pickleFileName = string_prompt("Please enter a filename that you wish to store data too:\n")
+        sampleManager = S3SamplesManager(pickleFileName)
         
-    xmlFile.save()
+        if bool_prompt("Manual Entry? (y/n):\n"):
+            sampleManager.start(s3Client)
+            
+            dataFile = open(pickleFileName, 'ab')
+            pickle.dump(sampleManager, dataFile)
+            dataFile.close()
+    
+            xmlFile.processSampleManagerSamples(sampleManager)   
+            xmlFile.save()
+        elif bool_prompt("Auto Entry? (y/n):\n"):
 
+            exit()
+
+
+    elif bool_prompt("Load existing IGV session? (y/n):\n"):
+        pickleFileName = string_prompt("Enter data filename:\n")
+        if bool_prompt("Would you like to regenerate this session? (y/n)\n"):
+            
+            dataFile = open(pickleFileName, 'rb')
+            pickleData = pickle.load(dataFile)
+            dataFile.close()
+
+            sampleManager = pickleData
+        
+            for S3Sample in sampleManager.S3Samples:
+                S3Sample.updateLinks(s3Client)
+            
+            dataFile = open(pickleFileName, 'wb')
+            pickle.dump(sampleManager, dataFile)
+            dataFile.close()
+
+            xmlFile.processSampleManagerSamples(sampleManager)
+            xmlFile.save()
 
 
 if __name__ == "__main__":
